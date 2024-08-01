e3617994477537b519bc2c370910afee7bd89054
kayapto <166363216+kayapto@users.noreply.github.com>
2024-08-01 18:59:33 -0400

    Update ScheduleViewModel.swift

--- AiFitnessApp/AiFitnessApp/ViewModels/ScheduleViewModel.swift
+++ AiFitnessApp/AiFitnessApp/ViewModels/ScheduleViewModel.swift
@@ -1,1 +1,10 @@
+// ScheduleViewModel.swift
+import Foundation
 
+class ScheduleViewModel: ObservableObject {
+    @Published var schedule = Schedule(days: [:])
+    
+    func generateSchedule(for user: User) {
+        // Call PredictionService and populate schedule
+    }
+}
