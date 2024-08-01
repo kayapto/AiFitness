0f3558a86b04ef0195e40a81996e8845b148193c
kayapto <166363216+kayapto@users.noreply.github.com>
2024-08-01 18:59:09 -0400

    Update PersonalDetailsViewModel.swift

--- AiFitnessApp/AiFitnessApp/ViewModels/PersonalDetailsViewModel.swift
+++ AiFitnessApp/AiFitnessApp/ViewModels/PersonalDetailsViewModel.swift
@@ -1,1 +1,13 @@
+// PersonalDetailsViewModel.swift
+import Foundation
+
+class PersonalDetailsViewModel: ObservableObject {
+    @Published var user = User(weight: 70.0, height: 170.0, age: 25, goal: "")
+    
+    func updateUserDetails(weight: Double, height: Double, age: Int) {
+        user.weight = weight
+        user.height = height
+        user.age = age
+    }
+}
 
