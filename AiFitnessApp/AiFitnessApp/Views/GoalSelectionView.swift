ad20e8657d9777ef86d51e216cb31160349ed41b
kayapto <166363216+kayapto@users.noreply.github.com>
2024-08-01 18:58:18 -0400

    Update GoalSelectionView.swift

--- AiFitnessApp/AiFitnessApp/Views/GoalSelectionView.swift
+++ AiFitnessApp/AiFitnessApp/Views/GoalSelectionView.swift
@@ -1,1 +1,16 @@
+import SwiftUI
 
+struct GoalSelectionView: View {
+    @State private var selectedGoal: String = "Weight Loss"
+    
+    var body: some View {
+        Picker("Select your goal", selection: $selectedGoal) {
+            Text("Weight Loss").tag("Weight Loss")
+            Text("Muscle Gain").tag("Muscle Gain")
+            Text("Endurance").tag("Endurance")
+        }
+        .pickerStyle(SegmentedPickerStyle())
+        .padding()
+        // Navigation to ScheduleView
+    }
+}
