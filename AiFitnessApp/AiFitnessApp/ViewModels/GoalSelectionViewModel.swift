cb226b2380649471499615976610c28c880a4dc7
kayapto <166363216+kayapto@users.noreply.github.com>
2024-08-01 18:59:22 -0400

    Update GoalSelectionViewModel.swift

--- AiFitnessApp/AiFitnessApp/ViewModels/GoalSelectionViewModel.swift
+++ AiFitnessApp/AiFitnessApp/ViewModels/GoalSelectionViewModel.swift
@@ -1,1 +1,11 @@
+// GoalSelectionViewModel.swift
+import Foundation
+
+class GoalSelectionViewModel: ObservableObject {
+    @Published var goal: String = "Weight Loss"
+    
+    func updateGoal(newGoal: String) {
+        goal = newGoal
+    }
+}
 
