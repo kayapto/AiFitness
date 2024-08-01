deab8442b68866536013fa41670edcceddbcbd26
kayapto <166363216+kayapto@users.noreply.github.com>
2024-08-01 18:58:33 -0400

    Update ScheduleView.swift

--- AiFitnessApp/AiFitnessApp/Views/ScheduleView.swift
+++ AiFitnessApp/AiFitnessApp/Views/ScheduleView.swift
@@ -1,1 +1,16 @@
+import SwiftUI
 
+struct ScheduleView: View {
+    var schedule: Schedule
+    
+    var body: some View {
+        List(schedule.days.keys.sorted(), id: \.self) { day in
+            Section(header: Text(day)) {
+                ForEach(schedule.days[day]!, id: \.self) { activity in
+                    Text(activity)
+                }
+            }
+        }
+        .navigationTitle("Your Weekly Schedule")
+    }
+}
